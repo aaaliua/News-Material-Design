6c4da945f658fdfccb5dc0ca3a8c0892327da435
liutao12311 <344072238@qq.com>
2014-11-11 17:46:22 +0800

    add AnimationSet

--- ttt/src/com/saulmm/material/fragments/oneFragment.java
+++ ttt/src/com/saulmm/material/fragments/oneFragment.java
@@ -2,11 +2,18 @@ package com.saulmm.material.fragments;
 
 import com.example.ttt.R;
 
+import android.R.animator;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 
 
@@ -18,4 +25,28 @@ public class oneFragment extends Fragment {
 
         return inflater.inflate(R.layout.one_drawer, container, false);
     }
+
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		ImageView v = (ImageView)view.findViewById(R.id.swipe);
+		
+		Animation translateAnimation = new TranslateAnimation(0.1f, -280.0f,0.9f,0.0f);  
+		Animation alphaAnimation = new AlphaAnimation(1.0f,0.0f);
+		alphaAnimation.setDuration(1000);
+		alphaAnimation.setRepeatCount(100);
+		alphaAnimation.setRepeatMode(Animation.RESTART);
+		
+		//设置动画时间               
+		translateAnimation.setDuration(1000);
+		translateAnimation.setRepeatCount(100);
+		translateAnimation.setRepeatMode(Animation.RESTART);
+		AnimationSet set = new AnimationSet(true);  
+		set.addAnimation(translateAnimation);
+		set.addAnimation(alphaAnimation);
+		                            v.startAnimation(set);
+	}
+    
+    
+    
 }
